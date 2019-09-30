285ac9da50dbb315f42297c06498535311d10d9e
Antlerjely97 <doducan.ltu14@gmail.com>
2019-09-30 14:00:32 +0700

    Larvavel 1

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">Admin Area - Khoa Phạm</a>
+        <a class="navbar-brand" href="index.html">Admin Đỗ An</a>
     </div>
     <!-- /.navbar-header -->
 
